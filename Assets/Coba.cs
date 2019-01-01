0d0da4087a2e030bd810b45d73ea9d79b7d066d3
Muhammad Choirul Anwar <jeruknipis8383@gmail.com>
2019-01-01 20:01:06 +0700

    Initial commit

---
+++ Assets/Coba.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coba : MonoBehaviour {
+	public float SecondInFullDay = 120f;
+	[Range(0,1)]
+	public float currentTime = 0;
+	[HideInInspector]
+	public float timeMultiplier = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentTime += (Time.deltaTime/SecondInFullDay)*timeMultiplier;
+		if (currentTime >= 1) {
+			currentTime = 0;
+		}
+	}
+}
